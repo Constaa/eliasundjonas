876ef95b081caae961873dbb36b6f2c9b5c40c6b
Elias <e.mueller24@gmx.de>
2021-02-24 16:46:18 +0100

    Artikel Controller Get, Post und Delete

--- src/main/java/com/example/demo/ArtikelController.java
+++ src/main/java/com/example/demo/ArtikelController.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
+
 @RestController
 @RequestMapping("/artikel")
 public class ArtikelController {
@@ -13,8 +15,26 @@ public class ArtikelController {
         this.artikel = artikelStorage;
     }
 
-    @GetMapping
-    public Artikel get(@RequestParam Long id) {
+    @GetMapping(path = "/{id}")
+    public Artikel get(@PathVariable Long id) {
         return artikel.get(id);
     }
+
+    @GetMapping
+    public Collection<Artikel> get(){
+        Collection<Artikel> artikelCollection = artikel.values();
+        return artikelCollection;
+    }
+
+    @PostMapping
+    public Artikel post(@RequestBody Artikel newArtikel) {
+        artikel.put(newArtikel.artikelNr, newArtikel);
+        return newArtikel;
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void delete(@PathVariable Long id) {
+        artikel.remove(id);
+    }
+
 }
