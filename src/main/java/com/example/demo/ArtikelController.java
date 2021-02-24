f39f51983b74187802711ad0fe0912075410153e
Constaa <jonas.dietsche@gmx.net>
2021-02-24 16:53:17 +0100

    artikelcontroller ergänzt

--- src/main/java/com/example/demo/ArtikelController.java
+++ src/main/java/com/example/demo/ArtikelController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
