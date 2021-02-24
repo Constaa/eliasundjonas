f1d2d1d36d2abe58cf33231cb80b0d24a0e050fb
Constaa <jonas.dietsche@gmx.net>
2021-02-24 16:45:04 +0100

    artikelcontroller ergänzt

--- src/main/java/com/example/demo/ArtikelController.java
+++ src/main/java/com/example/demo/ArtikelController.java
@@ -1,4 +1,48 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/artikel")
 public class ArtikelController {
+
+    private ArtikelStorage artikel;
+
+    @Autowired ArtikelController(ArtikelStorage artikelStorage) {
+        this.artikel = artikelStorage;
+    }
+
+    @GetMapping(path="/{id}")
+    public Artikel get(@PathVariable Long id) {
+        return artikel.get(id);
+    }
+    @GetMapping
+    public Collection<Artikel> get(){
+        Collection<Artikel> artikelCollection = artikel.values();
+        return artikelCollection;
+    }
+    @PutMapping(path = "/{id}")
+    public Artikel put(@PathVariable Long id,
+                         @RequestBody Artikel newArtikel) {
+        Artikel art = artikel.get(id);
+        if (art != null) {
+            if (newArtikel.artikelNr!=null)
+                art.artikelNr = newArtikel.artikelNr;
+            if (newArtikel.bezeichnung!=null)
+                art.bezeichnung = newArtikel.bezeichnung;
+            if (newArtikel.einzelpreis!=null)
+                art.einzelpreis = newArtikel.einzelpreis;
+            return newArtikel;
+        } else {
+            this.artikel.put(id, newArtikel);
+            return newArtikel;
+        }
+    }
+
+
 }
