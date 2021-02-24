cbb8bfc084052a768cf113e8d6af66d6af0e4bbb
Elias <e.mueller24@gmx.de>
2021-02-24 15:50:25 +0100

    Artikel Controller initial

--- src/main/java/com/example/demo/Artikel.java
+++ src/main/java/com/example/demo/Artikel.java
@@ -1,17 +1,22 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Artikel {
+
   public Long artikelNr;
   public String bezeichnung;
   public Double einzelpreis;
 
-    public Artikel() {
+  public Artikel() {
 
-    }
-    public Artikel(Long artikelNr, String bezeichnung, Double einzelpreis) {
-      this.artikelNr = artikelNr;
-      this.bezeichnung = bezeichnung;
-      this.einzelpreis = einzelpreis;
-    }
+  }
+
+  public Artikel(Long artikelNr, String bezeichnung, Double einzelpreis) {
+    this.artikelNr = artikelNr;
+    this.bezeichnung = bezeichnung;
+    this.einzelpreis = einzelpreis;
+  }
 
 }
--- src/main/java/com/example/demo/ArtikelController.java
+++ src/main/java/com/example/demo/ArtikelController.java
@@ -1,4 +1,20 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/artikel")
 public class ArtikelController {
+
+    private ArtikelStorage artikel;
+
+    @Autowired ArtikelController(ArtikelStorage artikelStorage) {
+        this.artikel = artikelStorage;
+    }
+
+    @GetMapping
+    public Artikel get(@RequestParam Long id) {
+        return artikel.get(id);
+    }
 }
