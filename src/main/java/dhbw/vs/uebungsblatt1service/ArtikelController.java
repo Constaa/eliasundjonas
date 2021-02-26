44cfdc4d9c4aaf30d54ba0d954998604741cdb8b
Elias <e.mueller24@gmx.de>
2021-02-26 09:39:20 +0100

    Package umbenennen

--- src/main/java/dhbw/vs/uebungsblatt1service/ArtikelController.java
+++ src/main/java/dhbw/vs/uebungsblatt1service/ArtikelController.java
@@ -1,7 +1,6 @@
-package com.example.demo;
+package dhbw.vs.uebungsblatt1service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
--- src/main/java/dhbw/vs/uebungsblatt1service/BestellungController.java
+++ src/main/java/dhbw/vs/uebungsblatt1service/BestellungController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package dhbw.vs.uebungsblatt1service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
